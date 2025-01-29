8cb64be77a728bd2114579ca462e9d32a6edf18e
PetLopJ <peterjlopez0911@gmail.com>
2025-01-29 15:07:25 -0500

    finished Lab 1 Don't have to do last one

---
+++ app/src/main/java/com/example/lab1/Employee.kt
@@ -0,0 +1,6 @@
+package com.example.lab1
+
+class Employee constructor( var id:Int = 0, var name:String = "" ) {
+
+
+}
--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         /*println("Hello World")
 
         val s : String = "Hello World"
@@ -27,21 +28,49 @@ class MainActivity : ComponentActivity() {
         for(i in 1..5) {
             sum += i
         }
-        println(sum)*/
+        println(sum)
 
         calcSum()
     }
 
-    fun calcSum() : Unit {
+    fun calcSum(start:Int, end:Int) : Int {
         var sum: Int = 0
-        for (i in 1..5) {
+        for (i in start..end) {
             sum += i
         }
-        println(sum)
-    }
+        return sum;
+        }
 
-    fun createSum(start:Int, end:Int) : Int {
-        var sum = 0;
+        println(calcSum(0, 5))
 
+        val e: Employee = Employee()
+        e.id = 1234
+        e.name = "John Smith"
+        println("ID: " + e.id)
+        println("Name: " + e.name)
+
+
+        var e1:Employee = Employee()
+        var e2:Employee = Employee()
+        var e3:Employee = Employee()
+
+        for(i in listOf<Employee>(e1,e2)) {
+            println(i)
+        }
+
+        for(i in listOf<Employee>(e3)) {
+            println(i)
+        }*/
+
+
+      listOf<Employee>()
+
+        var e1:Employee = Employee()
+        var e2:Employee = Employee()
+        var e3:Employee = Employee()
+
+       for (i in listOf<Employee>(e1,e2,e3)){
+           println(i)
+       }
     }
 }
